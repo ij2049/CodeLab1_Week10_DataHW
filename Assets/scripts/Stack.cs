8497b3fbcc4d8ed797d32a4a080f04f7119b5bbc
ij2049 <70619931+ij2049@users.noreply.github.com>
2021-04-06 01:35:36 +0900

    I thought I understood....but still confused hahahaha

---
+++ Assets/scripts/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stack : MonoBehaviour
+{
+    // public InputField input;
+    
+
+    private Stack<string> planner = new Stack<string>();
+    public Text display;
+    public Text result;
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        display.text = "what did you do today?\n \n a: sleep   b: watch movie  c: work out     d: playing games    e: homeworks \n f: exit";
+        
+        
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            planner.Push("sleep");
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            planner.Push("watch movie");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            planner.Push("work out");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            planner.Push("playing games");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            planner.Push("home work");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            result.text += "\n" + planner.Pop();
+        }
+    }
+
+}
